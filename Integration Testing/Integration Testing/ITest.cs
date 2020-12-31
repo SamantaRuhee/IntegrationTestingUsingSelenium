6ac84a952f46579c76e8965fa2b5fce3f8b5c1e6
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-12-31 19:36:03 +0600

    Selenium task

---
+++ Integration Testing/Integration Testing/ITest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace Inegration_Testing
+{
+    [TestFixture]
+    public class LoginTestElp
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheLoginTestElpTest()
+        {
+            driver.Navigate().GoToUrl("https://elp.duetbd.org/login/index.php");
+            driver.FindElement(By.Id("username")).Click();
+            driver.FindElement(By.Id("username")).Clear();
+            driver.FindElement(By.Id("username")).SendKeys("170042064");
+            driver.FindElement(By.Id("password")).Click();
+            driver.FindElement(By.Id("password")).Clear();
+            driver.FindElement(By.Id("password")).SendKeys("Abc.1234");
+            driver.FindElement(By.Id("loginbtn")).Click();
+            driver.FindElement(By.Id("action-menu-toggle-1")).Click();
+            driver.FindElement(By.LinkText("Log out")).Click();
+        }
+    }
+}
